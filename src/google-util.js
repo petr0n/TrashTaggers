9f8d21eb7d19165b07b5f5453899eed4c7444663
petr0n <petron@gmail.com>
2019-05-09 10:51:37 -0500

    more updates

--- src/google-util.js
+++ src/google-util.js
@@ -6,34 +6,10 @@ const googleConfig = {
   redirect: 'https://trashtaggers.heroku.com' // this must match your google api settings
 };
 
-/**
- * This scope tells google what information we want to request.
- */
 const defaultScope = [
     'https://www.googleapis.com/auth/plus.me',
     'https://www.googleapis.com/auth/userinfo.email',
   ];
-  
-  /**
-   * Get a url which will open the google sign-in page and request access to the scope provided (such as calendar events).
-   */
-  function getConnectionUrl(auth) {
-    return auth.generateAuthUrl({
-      access_type: 'offline',
-      prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time signs in
-      scope: defaultScope
-    });
-  }
-  
-  /**
-   * Create the google url to be sent to the client.
-   */
-  function urlGoogle() {
-    const auth = createConnection(); // this is from previous step
-    const url = getConnectionUrl(auth);
-    return url;
-  }
-
   
   /*************/
   /** HELPERS **/
