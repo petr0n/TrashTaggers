ddafd87bbeb8bda6054511c1c44985d0e02353de
petr0n <petron@gmail.com>
2019-05-09 14:34:34 -0500

    more updates6

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -36,7 +36,13 @@ passport.use(new GoogleStrategy({
 //   the user to google.com.  After authorization, Google will redirect the user
 //   back to this application at /auth/google/callback
 router.get('/auth/google',
-	passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+	passport.authenticate('google', { 
+		scope: [
+			'https://www.googleapis.com/auth/userinfo.profile',
+			'https://www.googleapis.com/auth/userinfo.email'
+		] 
+	})
+);
 
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
