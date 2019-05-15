97d2ff79def86a9a80950f8954733e4ea418e772
petr0n <petron@gmail.com>
2019-05-14 20:52:34 -0500

    fix datetime on events

--- public/js/trash.js
+++ public/js/trash.js
@@ -81,5 +81,15 @@ $(function () {
     });// ==> end join an event
 
 
+    $.get('https://www.instagram.com/explore/tags/trashtag/?__a=1', function (data, status) {
+      for (var i = 0; i < 6; i++) {
+        var isActive = i == 0 ? " active" : "";
+        var $this = data.graphql.hashtag.edge_hashtag_to_media.edges[i].node;
+        var slide = $('<div class="carousel-item ' + isActive + '" data-interval="5000"></div>');
+        slide.append('<img class="d-block w-100" src="' + $this.thumbnail_resources[2].src + '"><div class="carousel-caption d-none d-md-block">' + $this.edge_media_to_caption.edges[0].node.text + '</div>');
+        // console.log($this.edge_media_to_caption.edges[0].node.text);
+        $('#images').append(slide);
+      }
+    });
     
 }) // ==> end CODE
